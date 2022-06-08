32775a624a10b4f9f11912ad4a0fb81aa9ca2ae8
Yujie Liu <yujie.liu.home@gmail.com>
2022-06-09 01:16:27 +0200

    [Init] First Upload

--- src/main/java/nl/utwente/proverb/controller/EnrichmentController.java
+++ src/main/java/nl/utwente/proverb/controller/EnrichmentController.java
@@ -2,6 +2,8 @@ package nl.utwente.proverb.controller;
 
 import lombok.extern.log4j.Log4j2;
 import nl.utwente.proverb.service.OntologyService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
@@ -20,6 +22,8 @@ public class EnrichmentController {
     @Resource
     private OntologyService ontologyService;
 
+    ConfigurableApplicationContext context;
+
     public Map<String, Boolean> enrichRepository(){
         var repoResources = ontologyService.getAllRepository();
         var result = new HashMap<String, Boolean>(repoResources.size());
@@ -27,17 +31,20 @@ public class EnrichmentController {
             var success = repositoryController.enrichGitHubRepoDetail(resource);
             var uri = resource.toString();
             result.put(uri, success);
-            try {
-                ontologyService.write();
-            }catch (IOException e){
-                log.error("Write to file fail: uri={}", uri);
-            }
         });
+        try {
+            ontologyService.write();
+        }catch (IOException e){
+            log.error("Write to file fail");
+        }
         return result;
     }
 
     @PostConstruct
-    void execute(){
+    void execution(){
+        log.info("Enrichment start");
         this.enrichRepository();
+        log.info("Application exit");
+        SpringApplication.exit(context, () -> 0);
     }
 }
